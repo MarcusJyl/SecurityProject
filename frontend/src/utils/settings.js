5c9d6d0a7adc82fcf0060e009862aa98eb3b28dd
GGGE99 <marc.g.ekstrom@gmail.com>
2021-04-07 10:35:02 +0200

    database fix

--- frontend/src/utils/settings.js
+++ frontend/src/utils/settings.js
@@ -1,3 +1,3 @@
-export const loginURL = "http://localhost:8080/jpareststarter";
+export const loginURL = "http://localhost:8080/test";
 
-export const jokeURL = "http://localhost:8080/jpareststarter/api/jokes";
+export const jokeURL = "http://localhost:8080/test/api/jokes";
