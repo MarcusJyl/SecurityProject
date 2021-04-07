a46f9298a1474171bfce5c09ce7ba8929cde4205
GGGE99 <marc.g.ekstrom@gmail.com>
2021-04-07 12:34:08 +0200

    Post

---
+++ backend/src/main/java/entities/Post.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author marcg
+ */
+@Entity
+@Table(name = "post")
+public class Post implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @JoinColumn(name = "user_id")
+    @NotNull
+    @ManyToOne()
+    private User user;
+    
+    @Size(min = 1, max = 40)
+    @Column(name = "title")
+    private String title;
+    
+    @Size(min = 1, max = 255)
+    @Column(name = "content")
+    private String Content;
+
+    public Post() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    
+}
